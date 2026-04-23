f2f4b56d2d7e04bfac0c95008dd008e6eaf12324
Ama-Dombawela <amadombawela@gmail.com>
2026-04-23 10:27:39 +0530

    feat: add ErrorMessage POJO for standardised exception mapper responses

---
+++ SmartCampusAPI/src/main/java/com/smartcampus/api/model/ErrorMessage.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.api.model;
+
+/**
+ *
+ * @author User
+ */
+public class ErrorMessage {
+
+    private String errorMessage;
+    private int errorCode;
+    private String documentation;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(String errorMessage, int errorCode, String documentation) {
+        this.errorMessage = errorMessage;
+        this.errorCode = errorCode;
+        this.documentation = documentation;
+    }
+
+    /**
+     * @return the errorMessage
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    /**
+     * @param errorMessage the errorMessage to set
+     */
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * @return the errorCode
+     */
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    /**
+     * @param errorCode the errorCode to set
+     */
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * @return the documentation
+     */
+    public String getDocumentation() {
+        return documentation;
+    }
+
+    /**
+     * @param documentation the documentation to set
+     */
+    public void setDocumentation(String documentation) {
+        this.documentation = documentation;
+    }
+
+}
